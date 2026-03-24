86018e3aafa87d33cc43efd85441f9c1ded26cad
Sagi Buchbinder Shadur <saroad2@gmail.com>
2026-03-23 22:01:31 -0400

    Elevator now has doors that can open and close

--- packages/elevator-simulator-core/src/elevator_simulator/core/elevator.py
+++ packages/elevator-simulator-core/src/elevator_simulator/core/elevator.py
@@ -1,9 +1,32 @@
+from math import fabs
+
+from elevator_simulator.core.constants import EPSILON
+
+
 class Elevator:
     def __init__(self):
         self.current_location = 0.0
-        self.seconds_per_floor = 1.0
+        self.seconds_per_floor = 1.5
+        self.seconds_to_open = 0.3
+        self.seconds_to_close = 0.5
         self.max_floor = 3
+        self.door_position = 1.0  # 1 means fully open; 0 means fully closed
         self.target_floor = None
+
+    @property
+    def need_to_move(self) -> bool:
+        return (
+            self.target_floor is not None
+            and fabs(self.target_floor - self.current_location) > EPSILON
+        )
+
+    @property
+    def door_is_open(self) -> bool:
+        return fabs(self.door_position - 1) < EPSILON
+
+    @property
+    def door_is_closed(self) -> bool:
+        return fabs(self.door_position) < EPSILON
 
     def set_target_floor(self, floor: int | None = None):
         if floor is None:
@@ -14,6 +37,28 @@ class Elevator:
         self.target_floor = floor
 
     def update(self, elapsed_seconds: float):
+        if self.need_to_move:
+            if not self.door_is_closed:
+                self.close_door(elapsed_seconds)
+            else:
+                self._update_location(elapsed_seconds)
+        else:
+            if not self.door_is_open:
+                self.open_door(elapsed_seconds)
+
+    def close_door(self, elapsed_seconds: float):
+        new_door_position = self.door_position - (
+            elapsed_seconds / self.seconds_to_close
+        )
+        self.door_position = max(new_door_position, 0.0)
+
+    def open_door(self, elapsed_seconds: float):
+        new_door_position = self.door_position + (
+            elapsed_seconds / self.seconds_to_open
+        )
+        self.door_position = min(new_door_position, 1.0)
+
+    def _update_location(self, elapsed_seconds: float):
         direction = self._get_direction()
         if direction != 0:
             next_location = (
--- packages/elevator-simulator-gui/src/elevator_simulator/gui/draw_methods.py
+++ packages/elevator-simulator-gui/src/elevator_simulator/gui/draw_methods.py
@@ -6,12 +6,15 @@ from elevator_simulator.gui.constants import (
     SCREEN_WIDTH,
     SCREEN_HEIGHT,
     LEVEL_HEIGHT,
+    LINE_WIDTH,
+    ELEVATOR_DOOR_PADDING,
 )
 
 
 class ElevatorSimulatorDrawer:
     def __init__(self):
         self.floor_font = pygame.font.SysFont("Arial", 30)
+        self.half_elevator_width = ELEVATOR_WIDTH // 2
 
     def draw_levels(self, max_level: int, screen):
         for i in range(0, max_level + 1):
@@ -20,14 +23,32 @@ class ElevatorSimulatorDrawer:
                 "black",
                 (0, SCREEN_HEIGHT - (i + 1) * LEVEL_HEIGHT),
                 (SCREEN_WIDTH, SCREEN_HEIGHT - (i + 1) * LEVEL_HEIGHT),
+                LINE_WIDTH,
             )
             text_surface = self.floor_font.render(f"Level {i}", True, "black")
             screen.blit(text_surface, (0, SCREEN_HEIGHT - (i + 1) * LEVEL_HEIGHT))
 
     def draw_elevator(self, elevator, screen):
-        rect = pygame.Rect(0, 0, ELEVATOR_WIDTH, ELEVATOR_HEIGHT)
-        rect.midbottom = (
+        full_door_rect = pygame.Rect(0, 0, ELEVATOR_WIDTH, ELEVATOR_HEIGHT)
+        full_door_rect.midbottom = (
             SCREEN_WIDTH // 2,
             SCREEN_HEIGHT - int(LEVEL_HEIGHT * elevator.current_location),
         )
-        pygame.draw.rect(screen, "red", rect)
+        self.draw_rect(rect=full_door_rect, fill_color="red", screen=screen)
+
+        door_width = self.half_elevator_width - int(
+            elevator.door_position * (self.half_elevator_width - ELEVATOR_DOOR_PADDING)
+        )
+
+        left_door_rect = pygame.Rect(0, 0, door_width, ELEVATOR_HEIGHT)
+        left_door_rect.bottomleft = full_door_rect.bottomleft
+        self.draw_rect(rect=left_door_rect, fill_color="gray", screen=screen)
+
+        right_door_rect = pygame.Rect(0, 0, door_width, ELEVATOR_HEIGHT)
+        right_door_rect.bottomright = full_door_rect.bottomright
+        self.draw_rect(rect=right_door_rect, fill_color="gray", screen=screen)
+
+    @classmethod
+    def draw_rect(cls, *, rect, fill_color, screen):
+        pygame.draw.rect(screen, fill_color, rect)
+        pygame.draw.rect(screen, "black", rect, LINE_WIDTH)
